2fb58ac3a5c97ceff431bf288461116c187a394b
illuminxry <56198341+illuminxry@users.noreply.github.com>
2023-02-20 09:15:25 +0800

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -12,29 +12,49 @@ exports.getRegister = (req, res) => {
 };
 exports.postRegister = (req, res) => {
       const { username, password } = req.body;
-      if (username){
-            var sql = "INSERT INTO `users` (`username`, `password`) VALUES ?";
-            var values = [username,password]; 
-            conn.query('SELECT * FROM users WHERE username = `username`',[username],function(error,results,fields){
-              if(results.length > 0){
-                  res.send('Username already taken');
-                  console.log("username already taken");
-              }
-              else{
-                conn.query(sql,[values],function(err,result){
-                  if (err) {
-                        throw err;
-                        
-                  } else {
-                        console.log("record inserted" + result.affectedRows);
-                  }
-                });
-              }
-              res.end();
-            });
-          }
-          else{
-            res.send('Please enter a username and password');
-            res.end();
-          }
+      conn.query('SELECT * FROM users where username = `username`',[username],function(error,results,fields){
+            if(results.length <= 0){
+                  res.send("Username already taken");
+            }
+            else{
+                  var sql = 'INSERT INTO `users` (`username`, `password`) VALUES (?,?)';
+                  var values = [username, password];
+                  conn.connect(function(err){
+                        conn.query(sql, values, function (err, result){
+                              if(err) throw err;
+                              console.log("entry added");
+                              conn.destroy();
+                              res.render('home');
+                        })
+                  });
+            }
+      }); 
 };
+// exports.postRegister = (req, res) => {
+//       const { username, password } = req.body;
+//       if (username){
+//             var sql = "INSERT INTO `users` (`username`, `password`) VALUES ?";
+//             var values = [username,password]; 
+//             conn.query('SELECT * FROM users WHERE username = `username`',[username],function(error,results,fields){
+//               if(results.length > 0){
+//                   res.send('Username already taken');
+//                   console.log("username already taken");
+//               }
+//               else{
+//                 conn.query(sql,[values],function(err,result){
+//                   if (err) {
+//                         throw err;
+                        
+//                   } else {
+//                         console.log("record inserted" + result.affectedRows);
+//                   }
+//                 });
+//               }
+//               res.end();
+//             });
+//           }
+//           else{
+//             res.send('Please enter a username and password');
+//             res.end();
+//           }
+// };
