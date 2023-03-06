8d2f25b03185ded18e4ba40ea9b68fe79e8d6849
illuminxry <secret.ian0926@gmail.com>
2023-03-06 20:56:21 +0800

    added comments

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -20,9 +20,11 @@ exports.postLogin = (req, res) => {
   if (email && password) {
 
     conn.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
-      var decryptedpw = decrypt(results[0].password, shiftkey);//results[0] to get the first element of the array result especially in the password column
+      var decryptedpw = decrypt(results[0].password, shiftkey);
+      //results[0] to get the first element of the array result especially in the password column
       if (results.length > 0 && decryptedpw === password) {
-        console.log("logged in as user email-> " + email,", user password-> " + decryptedpw , ", user encrypted pw -> " + results[0].password);
+        console.log("logged in as user email-> " + email,", user password-> " 
+        + decryptedpw , ", user encrypted pw -> " + results[0].password);
         res.render('home');
       }
       else {
