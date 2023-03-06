5a37df4d1f8431103ad5ab6ce8dc526a4f32ee94
illuminxry <secret.ian0926@gmail.com>
2023-03-06 19:44:38 +0800

    updated console.log to check logged in user

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,4 @@
-const { decrypt } = require('caesar-encrypt');
+const { decrypt } = require('caesar-encrypt'); //import caesar-encrypt
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
@@ -7,7 +7,7 @@ const conn = mysql.createConnection({
   user: 'root',
   password: ''
 });
-const shiftkey = 10;
+const shiftkey = 10; // shift key to be used in caesar cipher
 
 exports.getLogin = (req, res) => {
   res.render('login');
@@ -20,11 +20,9 @@ exports.postLogin = (req, res) => {
   if (email && password) {
 
     conn.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
-      var decryptedpw = decrypt(results[0].password, shiftkey);
+      var decryptedpw = decrypt(results[0].password, shiftkey);//results[0] to get the first element of the array result especially in the password column
       if (results.length > 0 && decryptedpw === password) {
-
-        console.log("user email->" + email," user password-> " + decryptedpw);
-
+        console.log("logged in as user email-> " + email,", user password-> " + decryptedpw , ", user encrypted pw -> " + results[0].password);
         res.render('home');
       }
       else {
