f75a704f12d060bc4c6cbd54059a1b1decb3ae16
illuminxry <secret.ian0926@gmail.com>
2023-03-06 19:34:06 +0800

    decrypts user pw from db and compares pw to input

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,36 +1,41 @@
+const { decrypt } = require('caesar-encrypt');
 const mysql = require("mysql");
 
 const conn = mysql.createConnection({
-      host: 'localhost',
-      database: 'userlogin',
-      user: 'root',
-      password: ''
+  host: 'localhost',
+  database: 'userlogin',
+  user: 'root',
+  password: ''
 });
+const shiftkey = 10;
 
 exports.getLogin = (req, res) => {
-    res.render('login');
+  res.render('login');
 };
 
 exports.postLogin = (req, res) => {
 
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (email && password){
+  if (email && password) {
 
-      conn.query('SELECT * FROM users WHERE email = ? AND password = ?',[email,password],function(error,results,fields){
-        if(results.length > 0){
-          console.log(email,password);
-          res.render('home');
-        }
-        else{
-          res.send('Incorrect Email and/or Password');
-          console.log(error);
-        }
-        res.end();
-      });
-    }
-    else{
-      res.send('Please enter a Email and password');
+    conn.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+      var decryptedpw = decrypt(results[0].password, shiftkey);
+      if (results.length > 0 && decryptedpw === password) {
+
+        console.log("user email->" + email," user password-> " + decryptedpw);
+
+        res.render('home');
+      }
+      else {
+        res.send('Incorrect Email and/or Password');
+        console.log(error);
+      }
       res.end();
-    }
+    });
+  }
+  else {
+    res.send('Please enter a Email and password');
+    res.end();
+  }
 };
