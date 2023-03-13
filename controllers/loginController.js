bc11902dec127aa846d5385fa284b80db7fd9c59
illuminxry <secret.ian0926@gmail.com>
2023-03-13 20:48:56 +0800

    fixed decryption

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,31 +1,30 @@
-const { decrypt } = require('caesar-encrypt'); //import caesar-encrypt
+const { decrypt } = require('caesar-encrypt'); 
+//import caesar-encrypt
 const mysql = require("mysql");
-
-const conn = mysql.createConnection({
-  host: 'localhost',
-  database: 'userlogin',
-  user: 'root',
-  password: ''
-});
-const shiftkey = 10; // shift key to be used in caesar cipher
 
 exports.getLogin = (req, res) => {
   res.render('login');
 };
 
 exports.postLogin = (req, res) => {
-
   const { email, password } = req.body;
 
+  const conn = mysql.createConnection({
+    host: 'localhost',
+    database: 'userlogin',
+    user: 'root',
+    password: ''
+  });
   if (email && password) {
 
     conn.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
-      var decryptedpw = decrypt(results[0].password, shiftkey);
-      //results[0] to get the first element of the array result especially in the password column
+      var decryptedpw = decrypt(results[0].password, results[0].shift);
+
       if (results.length > 0 && decryptedpw === password) {
         console.log("logged in as user email-> " + email,", user password-> " 
         + decryptedpw , ", user encrypted pw -> " + results[0].password);
-        res.render('home');
+        
+        res.redirect('home');
       }
       else {
         res.send('Incorrect Email and/or Password');
