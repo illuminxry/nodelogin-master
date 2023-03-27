6bd120c24082936d69edf0ec3665d3e4ff53c156
Christian Emmanuel A. Pastrana <56198341+illuminxry@users.noreply.github.com>
2023-03-27 15:41:01 +0800

    updated homeroutes march27

--- routes/home.js
+++ routes/home.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const homeController = require('../controllers/homeController');
 
 router.get('/home', homeController.getHome);
-
+router.post('/home', homeController.postHome);
 module.exports = router;
